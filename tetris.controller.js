0345ef51015340bd24bb68d9c615a55c2e791b3f
Sampson Crowley <sampsonsprojects@gmail.com>
2017-01-11 15:35:53 -0700

    add timing loop for drop();

--- tetris.controller.js
+++ tetris.controller.js
@@ -7,6 +7,7 @@ TETRIS.Controller.init = function init(){
   this.model.init();
   this.view.init();
   this.animationSpeed();
+  this.then = Date.now();
   this.animate();
 };
 
@@ -15,8 +16,17 @@ TETRIS.Controller.animationSpeed = function animationSpeed(){
   window.requestAnimationFrame = requestAnimationFrame;
 };
 
+TETRIS.Controller.drop = function drop(){
+  this.now = Date.now() - this.then
+  if(this.now > 1000){
+    // this.model.drop();
+    this.then = Date.now()
+  }
+};
+
 TETRIS.Controller.animate = function animate(){
   requestAnimationFrame(function(){TETRIS.Controller.animate()})
   // this.model.move();
+  this.drop();
   this.view.renderEntities({shapes: this.model.shapes});
 };
