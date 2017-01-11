03c4c25529cf8946b018373f725cfd0ef9a779d0
Sampson Crowley <sampsonsprojects@gmail.com>
2017-01-11 15:39:03 -0700

    basic drop function

--- tetris.model.js
+++ tetris.model.js
@@ -6,6 +6,7 @@ TETRIS.Model = TETRIS.Model || {}
 
 TETRIS.Model.init = function init(){
   this.shapes = [new this.Bar()]
+  this.activeShape = this.shapes[0];
 };
 
 TETRIS.Model.descentRate = 1;
@@ -14,6 +15,7 @@ TETRIS.Model.Grid = {
   height: window.innerHeight, // TODO extract to view
   width: (this.height / 2),
   rows: 20,
+  rowHeight: (window.innerHeight / 20)
   columns: 10
 };
 
@@ -21,6 +23,6 @@ TETRIS.Model.offset = function offset(shapeDiameter) {
   return (this.Grid.columns / 2) - Math.floor(shapeDiameter / 2);
 };
 
-TETRIS.Model.move = function move() {
-  
+TETRIS.Model.drop = function drop() {
+  this.activeShape.y += this.Grid.rowHeight
 };
