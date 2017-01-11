93735bf1276924a459acd9bf4908e937e7eed583
Sampson Crowley <sampsonsprojects@gmail.com>
2017-01-11 14:05:32 -0700

    rotation

--- tetris.model.js
+++ tetris.model.js
@@ -30,6 +30,12 @@ TETRIS.Model.Square = function Square(options) {
   this.y = this.ry * this.diameter;
   this.filled = options.filled
 }
+TETRIS.Model.prototype.updateCoords = function updateCoords(x, y){
+  this.rx = x;
+  this.ry = y;
+  this.x = this.rx * this.diameter;
+  this.y = this.ry * this.diameter;
+};
 
 TETRIS.Model.Shape = function Shape(options) {
   this.orientation = 0;
@@ -44,7 +50,7 @@ TETRIS.Model.Shape = function Shape(options) {
     var squareOptions = {};
     for(var c = 0; c < this.diameter; c++){
       squareOptions = { x: c + TETRIS.Model.offset(this.diameter), y: r };
-      if(options.filled[c + "_" + r]) {
+      if(options.filled[0][c + "_" + r]) {
         squareOptions.filled = true;
       }
       this.matrix[r + c + (r*(this.diameter-1))] = new TETRIS.Model.Square(squareOptions);
@@ -56,15 +62,50 @@ TETRIS.Model.Shape = function Shape(options) {
 
 TETRIS.Model.Shape.prototype.rotate = function rotate() {
   //TODO
+  /*
+  0  | 1  | 2  | 3
+  4  | 5  | 6  | 7
+  8  | 9  | 10 | 11
+  12 | 13 | 14 | 15
+
+  3  | 7  | 11 | 15
+  2  | 6  | 10 | 14
+  1  | 5  | 9  | 13
+  0  | 4  | 8  | 12
+  for n = SIZE - 1 to 0
+    for i = 0 to SIZE**2-1
+      this.matrix.["r270"][i] = this.matrix[i + n]
+  */
 };
 
 TETRIS.Model.Bar = function Bar() {
   TETRIS.Model.Shape.call(this, {
     diameter: 4,
-    filled: { "0_1": true,
-      "1_1": true,
-      "2_1": true,
-      "3_1": true
+    filled: {
+      0: {
+        "0_1": true,
+        "1_1": true,
+        "2_1": true,
+        "3_1": true
+      },
+      1: {
+        "2_0": true,
+        "2_1": true,
+        "2_2": true,
+        "2_3": true
+      },
+      2: {
+        "1_0": true,
+        "1_1": true,
+        "1_2": true,
+        "1_3": true
+      },
+      3: {
+        "0_2": true,
+        "1_2": true,
+        "2_2": true,
+        "3_2": true
+      }
     }
   });
 };
